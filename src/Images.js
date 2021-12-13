25ed5a2c6a43ea6b6b553cf6b943edb4f535f6da
Igorich1 <kravets2004@gmail.com>
2021-12-13 14:16:20 +0200

    add images

---
+++ src/Images.js
@@ -0,0 +1,21 @@
+import Image01 from "./mems/01.png"
+import Image02 from "./mems/02.png"
+import Image03 from "./mems/03.png"
+import Image04 from "./mems/04.png"
+import Image05 from "./mems/05.png"
+import Image06 from "./mems/06.png"
+import Image07 from "./mems/07.jpeg"
+import Image08 from "./mems/08.png"
+import Image09 from "./mems/09.png"
+import Image10 from "./mems/10.png"
+import Image11 from "./mems/11.png"
+import Image12 from "./mems/12.png"
+import Image13 from "./mems/13.png"
+import Image14 from "./mems/14.png"
+import Image15 from "./mems/15.png"
+import Image16 from "./mems/16.png"
+import Image17 from "./mems/17.png"
+import Image18 from "./mems/18.png"
+
+const Images = [ Image01, Image02, Image03, Image04, Image05, Image06, Image07, Image08, Image09, Image10, Image11, Image12, Image13, Image14, Image15, Image16, Image17, Image18 ];
+export default Images;
